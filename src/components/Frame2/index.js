fc5cc167361774356b939268e041de5d7589660f
Yuhaigiub <buigiahuynt@gmail.com>
2023-06-29 15:50:21 +0700

    bug fix add scroll watch

--- src/components/Frame2/index.js
+++ src/components/Frame2/index.js
@@ -1,27 +1,63 @@
 import Swiper from "swiper";
 import SwiperAnimation from "@cycjimmy/swiper-animation";
+import bgVideo from "../Frame1/assets/frame1_bg_video.mp4";
 
 export function runFrame2() {
-	const swiperAnimation = new SwiperAnimation();
-	console.log(swiperAnimation);
-	const frame2CharacterSwiper = new Swiper(".frame2-swiper-character", {
-		direction: "horizontal",
-		loop: true,
-		effect: "fade",
-		fadeEffect: {
-			crossFade: true,
-		},
-		on: {
-			slideChange: function () {
-				swiperAnimation.init(this).animate();
-			},
-		},
-	});
-
 	// buttons and decor balls
 	const decorBalls = $(".decor-ball");
 	const maleButton = $("#frame2-male-button");
 	const femaleButton = $("#frame2-female-button");
+
+	const obsCallback = (mutationList, observer) => {
+		let isActive = false;
+		mutationList.map((mutation) => {
+			if (mutation.target.classList.contains("swiper-slide-active")) {
+				isActive = true;
+			}
+		});
+		if (isActive) {
+			maleButton.attr("active", true);
+			femaleButton.attr("active", false);
+			decorBalls.eq(0).show();
+			decorBalls.eq(3).show();
+			decorBalls.eq(1).hide();
+			decorBalls.eq(2).hide();
+		} else {
+			maleButton.attr("active", false);
+			femaleButton.attr("active", true);
+			decorBalls.eq(0).hide();
+			decorBalls.eq(3).hide();
+			decorBalls.eq(1).show();
+			decorBalls.eq(2).show();
+		}
+	};
+	const observer = new MutationObserver(obsCallback);
+	observer.observe($(".frame2-swiper-character .swiper-slide").get(0), {
+		childList: true,
+		attributes: true,
+		characterData: true,
+	});
+
+	const swiperAnimation = new SwiperAnimation();
+	const frame2CharacterSwiper = new Swiper(".frame2-swiper-character", {
+		direction: "horizontal",
+		loop: true,
+		effect: "fade",
+		centeredSlides: true,
+		followFinger: false,
+		oneWayMovement: true,
+		fadeEffect: {
+			crossFade: true,
+		},
+		on: {
+			init: function (swiper) {
+				swiperAnimation.init(this).animate();
+			},
+			slideChange: function (swiper) {
+				swiperAnimation.init(this).animate();
+			},
+		},
+	});
 
 	maleButton.on("click", function (event) {
 		maleButton.attr("active", true);
@@ -42,4 +78,11 @@ export function runFrame2() {
 		decorBalls.eq(2).show();
 		frame2CharacterSwiper.slideTo(1);
 	});
+
+	//
+	const video = $("#frame2-video-container video");
+	const src = document.createElement("source");
+	src.src = bgVideo;
+	src.type = "video/mp4";
+	video.append(src);
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
  *
  */
 // scale root
-
+import ScrollWatch from "scrollwatch";
 import { initiatePopup } from "./components/Popup";
 
 import { runHeaderMobile } from "./components/HeaderMobile";
@@ -15,7 +15,7 @@ import { runFrame1 } from "./components/Frame1";
 import { runFrame2 } from "./components/Frame2";
 import { runFrame3 } from "./components/Frame3";
 import { runFrame5 } from "./components/Frame5";
-import { isInView, animateCSS } from "./import_assets";
+import { animateCSS } from "./import_assets";
 
 $(function () {
 	// ready
@@ -72,78 +72,43 @@ $(function () {
 	runFrame3();
 	runFrame5();
 
+	const frame2Info = $("#frame2-info");
+	const frame2Swiper = $(".frame2-swiper-character");
+	const frame2Video = $("#frame2-video-container");
+
+	const frame3Swiper = $(".frame3-swiper");
+	const frame4Buttons = $("#frame4-button-container .frame4-button");
+
+	const sw = new ScrollWatch({
+		onElementInView: function (data) {
+			switch (data.el) {
+				case frame2Info.get(0):
+					animateCSS(frame2Info, "animate__fadeInLeftBig");
+					return;
+				case frame2Swiper.get(0):
+					animateCSS(frame2Swiper, "animate__fadeInUpBig");
+					return;
+				case frame2Video.get(0):
+					animateCSS(frame2Video, "animate__fadeInRightBig");
+					return;
+				// frame 3
+				case frame3Swiper.get(0):
+					animateCSS(frame3Swiper, "animate__fadeInUpBig");
+					return;
+			}
+
+			let done = false;
+			frame4Buttons.each(function (index, el) {
+				if (data.el === el) {
+					animateCSS(el, "animate__fadeInLeftBig");
+					done = true;
+					return false;
+				}
+			});
+			if (done) return;
+		},
+	});
+
 	window.addEventListener("load", scaleRoot);
 	window.addEventListener("resize", scaleRoot);
-
-	function frame2InView() {
-		const frame2Info = $("#frame2-info");
-		const frame2Swiper = $(".frame2-swiper-character");
-		const frame2Video = $("#frame2-video-container");
-		if (isInView(frame2Info, $(window))) {
-			animateCSS(frame2Info, "animate__fadeInLeftBig");
-			animateCSS(frame2Swiper, "animate__fadeInUpBig");
-			animateCSS(frame2Video, "animate__fadeInRightBig");
-
-			window.removeEventListener("load", frame2InView);
-			window.removeEventListener("scroll", frame2InView);
-		}
-	}
-
-	function frame3InView() {
-		const frame3Swiper = $(".frame3-swiper");
-		if (isInView(frame3Swiper, $(window))) {
-			animateCSS(frame3Swiper, "animate__fadeInUpBig");
-
-			window.removeEventListener("load", frame3InView);
-			window.removeEventListener("scroll", frame3InView);
-		}
-	}
-
-	function frame4InView() {
-		const frame4ButtonContainer = $("#frame4-button-container");
-		const frame4Buttons = $("#frame4-button-container .frame4-button");
-
-		if (isInView(frame4ButtonContainer, $(window)) && $(window).width() >= 750) {
-			frame4Buttons.hide();
-			frame4Buttons.each(function (index, element) {
-				setTimeout(() => {
-					$(element).show();
-					animateCSS(element, "animate__fadeInLeftBig");
-				}, index * 100);
-			});
-
-			window.removeEventListener("load", frame4InView);
-			window.removeEventListener("scroll", frame4InView);
-		}
-	}
-
-	function frame5InView() {
-		const frame5ButtonContainer = $("#frame5-button-container");
-		const frame5Buttons = $("#frame5-button-container .frame5-button");
-
-		if (isInView(frame5ButtonContainer, $(window)) && $(window).width() >= 750) {
-			frame5Buttons.hide();
-			frame5Buttons.each(function (index, element) {
-				setTimeout(() => {
-					$(element).show();
-					animateCSS(element, "animate__fadeInUpBig");
-				}, index * 100);
-			});
-
-			window.removeEventListener("load", frame5InView);
-			window.removeEventListener("scroll", frame5InView);
-		}
-	}
-
-	window.addEventListener("load", frame2InView);
-	window.addEventListener("scroll", frame2InView);
-
-	window.addEventListener("load", frame3InView);
-	window.addEventListener("scroll", frame3InView);
-
-	window.addEventListener("load", frame4InView);
-	window.addEventListener("scroll", frame4InView);
-
-	window.addEventListener("load", frame5InView);
-	window.addEventListener("scroll", frame5InView);
 });
