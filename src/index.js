6fd60f82409a4b41fd1bc5a087aaaab13cd67792
BuiGiaHuy <buigiahuynt@gmail.com>
2023-07-02 20:13:39 +0700

    bring scale root out of jquery ready() build to dist

--- src/index.js
+++ src/index.js
@@ -17,53 +17,53 @@ import { runFrame3 } from "./components/Frame3";
 import { runFrame5 } from "./components/Frame5";
 import { animateCSS } from "./import_assets";
 
+// custom
+const mobileHeaderContainer = $("#mobile-header-container").get(0);
+mobileHeaderContainer.style.transformOrigin = "top left";
+
+const backToTop = $("#back-to-top-button").get(0);
+
+//
+const outerRoot = document.getElementById("outer-root");
+const root = document.getElementById("root");
+
+document.body.style.padding = 0;
+document.body.style.margin = 0;
+document.body.style.width = "100vw";
+document.body.style.height = "fit-content";
+document.body.style.overflowX = "hidden";
+
+outerRoot.style.width = "fit-content";
+outerRoot.style.overflow = "hidden";
+
+root.style.transformOrigin = "top left";
+root.style.overflow = "hidden";
+
+let mode, width, height, ratio;
+
+function scaleRoot() {
+	// check on every trigger
+	mode = window.innerWidth <= 768 ? "mobile" : "pc";
+	width = root.offsetWidth;
+	height = root.offsetHeight;
+	ratio = width / height;
+
+	const desiredWidth = window.innerWidth;
+	const desiredHeight = desiredWidth / ratio;
+
+	const ratioW = desiredWidth / width;
+	const ratioH = desiredHeight / height;
+
+	outerRoot.style.width = `${desiredWidth}px`;
+	outerRoot.style.height = `${desiredHeight}px`;
+
+	root.style.transform = `scale(${ratioW}, ${ratioH})`;
+	mobileHeaderContainer.style.transform = `scale(${ratioW}, ${ratioH})`;
+	backToTop.style.transform = `scale(${ratioW}, ${ratioH})`;
+}
+
 $(function () {
 	// ready
-
-	// custom
-	const mobileHeaderContainer = $("#mobile-header-container").get(0);
-	mobileHeaderContainer.style.transformOrigin = "top left";
-
-	const backToTop = $("#back-to-top-button").get(0);
-
-	//
-	const outerRoot = document.getElementById("outer-root");
-	const root = document.getElementById("root");
-
-	document.body.style.padding = 0;
-	document.body.style.margin = 0;
-	document.body.style.width = "100vw";
-	document.body.style.height = "fit-content";
-	document.body.style.overflowX = "hidden";
-
-	outerRoot.style.width = "fit-content";
-	outerRoot.style.overflow = "hidden";
-
-	root.style.transformOrigin = "top left";
-	root.style.overflow = "hidden";
-
-	let mode, width, height, ratio;
-
-	function scaleRoot() {
-		// check on every trigger
-		mode = window.innerWidth <= 768 ? "mobile" : "pc";
-		width = root.offsetWidth;
-		height = root.offsetHeight;
-		ratio = width / height;
-
-		const desiredWidth = window.innerWidth;
-		const desiredHeight = desiredWidth / ratio;
-
-		const ratioW = desiredWidth / width;
-		const ratioH = desiredHeight / height;
-
-		outerRoot.style.width = `${desiredWidth}px`;
-		outerRoot.style.height = `${desiredHeight}px`;
-
-		root.style.transform = `scale(${ratioW}, ${ratioH})`;
-		mobileHeaderContainer.style.transform = `scale(${ratioW}, ${ratioH})`;
-		backToTop.style.transform = `scale(${ratioW}, ${ratioH})`;
-	}
 
 	initiatePopup();
 	runHeaderMobile();
@@ -108,7 +108,7 @@ $(function () {
 			if (done) return;
 		},
 	});
-
-	window.addEventListener("load", scaleRoot);
-	window.addEventListener("resize", scaleRoot);
 });
+
+window.addEventListener("load", scaleRoot);
+window.addEventListener("resize", scaleRoot);
